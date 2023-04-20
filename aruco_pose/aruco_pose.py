3bc3ff83f4701e365ab59178f191d8ac445033c2
denis-lab-pc <den.davletshin999@gmail.com>
2023-04-20 19:50:47 +0300

    online aruco detection with ROS

--- aruco_pose/aruco_pose.py
+++ aruco_pose/aruco_pose.py
@@ -1,136 +1,238 @@
 import yaml
+import time 
+from pathlib import Path
 
 import cv2 as cv
 import numpy as np
 
-camera_params_file = 'configs/realsense_d435i_640.yaml'
-with open(camera_params_file) as f:
-    camera_params = yaml.load(f, Loader=yaml.FullLoader)
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+from geometry_msgs.msg import TransformStamped
 
-intrinsics = np.asarray(camera_params['intrinsics'], dtype=np.float32)
-dist_coeffs = np.asarray(camera_params['distortion'], dtype=np.float32)
-# cv.VideoCapture inputVideo;
-# inputVideo.open(0);
 
-# cv.Mat cameraMatrix, distCoeffs;
+class StampedImagesCollector:
+    def __init__(self, img_topic: str, mode='video', img_path=None, rate=1):
+        self.img_topic = img_topic
+        self.t_start = time.time()
+        self.mode = mode
 
-markerLength = 0.036
-aruco_params = cv.aruco.DetectorParameters_create()
-aruco_dict = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_4X4_50)
-print(dir(aruco_dict))
-# cap = cv.VideoCapture()
-# cap.open(4)
+        self.cv_bridge = CvBridge()
+
+        self.rate = rate
+        self.t_start = time.time()
+        self.last_time = None
+
+        self.last_frame = None
+        self.last_det_img = None
+        self.times_arr = []
+
+        camera_params_file = 'configs/realsense_d435i_640.yaml'
+        with open(camera_params_file) as f:
+            camera_params = yaml.load(f, Loader=yaml.FullLoader)
+
+        self.intrinsics = np.asarray(camera_params['intrinsics'], dtype=np.float32)
+        self.dist_coeffs = np.asarray(camera_params['distortion'], dtype=np.float32)
+
+        self.markerLength = 0.036
+        self.aruco_params = cv.aruco.DetectorParameters_create()
+        self.aruco_dict = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_4X4_50)
+            
+    
+    def start_listen(self):
+        if self.mode == 'video':
+            cv.waitKey(2000)
+            print('listener started')
+            rospy.init_node('aruco_detector', anonymous=True)
+
+            self.image_sub = rospy.Subscriber( self.img_topic, Image, self._image_callback)
+            self.det_pub = rospy.Publisher(str(Path(img_topic).parent/"aruco_det_img"), Image, queue_size=1)
+            time.sleep(2)
+            self.timer = rospy.Timer(rospy.Duration(1/self.rate), self.timer_callback)
+            rospy.spin()
+        else:
+            self.last_frame = cv.imread(img_path)
+
+
+    def _image_callback(self, msg):
+        try:
+            img = self.cv_bridge.imgmsg_to_cv2(msg, "bgr8")
+        except CvBridgeError:
+            rospy.logerr("Error: cannot convert from img_msg to cv2_image")
+            self.last_frame = None
+            self.last_time = None
+
+        self.last_time = time.time() - self.t_start
+        self.last_frame = img
+        self.times_arr.append(self.last_time)
+
+    def timer_callback(self, event=None):
+        self.aruco_detect()
+
+
+    def aruco_detect(self):
+        print('detecting arucos')
+        (corners, ids, rejected) = cv.aruco.detectMarkers( self.last_frame, self.aruco_dict,
+                parameters=self.aruco_params)
+
+        aruco_pts = np.zeros((4,3), dtype=np.float32)
+        markerLength = self.markerLength
+        aruco_pts[0] = [-markerLength/2, markerLength/2, 0]
+        aruco_pts[1] = [markerLength/2, markerLength/2, 0]
+        aruco_pts[2] = [markerLength/2, -markerLength/2, 0]
+        aruco_pts[3] = [-markerLength/2, -markerLength/2, 0]
+
+        self.last_det_img = self.last_frame.copy()
+        if (ids is not None):
+            nMarkers = ids.size
+            print(f'detected {nMarkers} markers')
+            print(f'ids: {ids}')
+            self.last_det_img = cv.aruco.drawDetectedMarkers(self.last_frame, corners, ids)
+
+            aruco_pos_arr = []
+            aruco_rot_arr = []
+            aruco_T_arr = []
+
+            for i, c in enumerate(corners):
+                retval, rvec, tvec = cv.solvePnP( aruco_pts, c, self.intrinsics, self.dist_coeffs)	
+                if retval:
+                    rot = np.zeros((3,3))
+                    cv.Rodrigues(rvec, rot)
+                    aruco_pos_arr.append(tvec)
+                    aruco_rot_arr.append(rvec)
+                    T = np.zeros((4,4))
+                    T[:3,:3] = rot
+                    T[:3,3] = tvec.reshape((3,))
+                    T[3,3] = 1
+                    aruco_T_arr.append(T)
+                    # rvecs, tvecs, _ = cv.aruco.estimatePoseSingleMarkers( corners, 0.18, intrinsics, dist_coeffs )
+                    print(f'tvec: {tvec}, rvec: {rvec}')
+                    self.last_det_img = cv.drawFrameAxes(self.last_det_img, self.intrinsics, self.dist_coeffs, rvec, tvec, 0.1)
+                # corners_galery_rel_galery = np.array([ [[galery_map_size[0] + padding_size], [galery_map_size[1] + padding_size], [0.]],
+                #                                        [[-padding_size], [galery_map_size[1] + padding_size], [0.]],
+                #                                        [[-padding_size], [-padding_size], [0.]],
+                #                                        [[galery_map_size[0] + padding_size], [-padding_size], [0.]] ])
+
+            # X rot
+            Rot_ArW = np.array([[1, 0, 0],
+                                [0, 0, 1],
+                                [0, -1, 0]])
+            
+            Trans_ArW = np.array([[-0.1,-0.1, 0.2]])
+            # Trans_ArW = np.array([[0,0,0]])
+            T_ArW = np.zeros((4,4))
+            T_ArW[:3,:3] = Rot_ArW
+            T_ArW[:3,3] = Trans_ArW
+            T_ArW[3,3] = 1
+            
+            # aruco_rot_m = np.zeros((3,3))
+            # cv.Rodrigues(aruco_rot_arr[2], aruco_rot_m)
+            # rvec_world =  aruco_rot_m @ aruco_to_world_rot_m
+            
+            ind = np.where(ids.flatten() == 0)
+            print('ind: ', ind)
+            if (ind[0].size != 0):
+                T_wc = aruco_T_arr[ind[0][0]] @ T_ArW
+
+                # print(rvec_world)
+                # self.last_det_img = cv.drawFrameAxes(self.last_det_img, intrinsics, dist_coeffs, rvec_world, aruco_pos_arr[2], 0.1)
+                self.last_det_img = cv.drawFrameAxes(self.last_det_img, self.intrinsics, self.dist_coeffs, T_wc[:3,:3], T_wc[:3,3], 0.1)
+
+            # if rvecs is not None :
+            #     for rvec, tvec, id in zip(rvecs, tvecs, ids) :
+            #         rotation_matrix = np.zeros(shape=(3,3))
+            #         cv.Rodrigues(rvec, rotation_matrix)
+        else:
+            print('didn\'t find arucos')
+        img_msg = self.cv_bridge.cv2_to_imgmsg(self.last_det_img, encoding="rgb8")
+        self.det_pub.publish(img_msg)
+        print('published_image')
+
+
 # corners, ids, rejectedImgPoints = aruco_detector.detectMarkers(img)
 
-print('hello')
+mode = 'video'
+img_path = 'aruco.png'
+img_topic = '/camera/color/image_raw'
+rate = 5
 
-img = cv.imread('arucos.png')
-print(img.shape)
-(corners, ids, rejected) = cv.aruco.detectMarkers( img, aruco_dict,
-            parameters=aruco_params)
-
-
-
-aruco_pts = np.zeros((4,3), dtype=np.float32)
-aruco_pts[0] = [-markerLength/2, markerLength/2, 0]
-aruco_pts[1] = [markerLength/2, markerLength/2, 0]
-aruco_pts[2] = [markerLength/2, -markerLength/2, 0]
-aruco_pts[3] = [-markerLength/2, -markerLength/2, 0]
-
-print(aruco_pts.dtype)
-print(corners[0].dtype)
-if (ids.size > 0):
-    img_det = img.copy()
-    img_det = cv.aruco.drawDetectedMarkers(img, corners, ids)
-    cv.imshow('det', img_det)
-    cv.waitKey(2000)
-
-    nMarkers = ids.size
-    aruco_pos_arr = []
-    aruco_rot_arr = []
-    for i, c in enumerate(corners):
-        retval, rvec, tvec = cv.solvePnP( aruco_pts, c, intrinsics, dist_coeffs)	
-        aruco_pos_arr.append(tvec)
-        aruco_rot_arr.append(rvec)
-        # rvecs, tvecs, _ = cv.aruco.estimatePoseSingleMarkers( corners, 0.18, intrinsics, dist_coeffs )
-        print(f'tvec: {tvec}, rvec: {rvec}')
-        img_det = cv.drawFrameAxes(img_det, intrinsics, dist_coeffs, rvec, tvec, 0.1)
-        # corners_galery_rel_galery = np.array([ [[galery_map_size[0] + padding_size], [galery_map_size[1] + padding_size], [0.]],
-        #                                        [[-padding_size], [galery_map_size[1] + padding_size], [0.]],
-        #                                        [[-padding_size], [-padding_size], [0.]],
-        #                                        [[galery_map_size[0] + padding_size], [-padding_size], [0.]] ])
-
-    aruco_to_world_rot_m = np.array([[1, 0, 0],
-                                     [0, 0, -1],
-                                     [0, 1, 0]])
-    
-    aruco_rot_m = np.zeros((3,3))
-    cv.Rodrigues(aruco_rot_arr[2], aruco_rot_m)
-    rvec_world =  aruco_rot_m @ aruco_to_world_rot_m
-    print(rvec_world)
-    img_det = cv.drawFrameAxes(img_det, intrinsics, dist_coeffs, rvec_world, aruco_pos_arr[2], 0.1)
-
-    # if rvecs is not None :
-    #     for rvec, tvec, id in zip(rvecs, tvecs, ids) :
-    #         rotation_matrix = np.zeros(shape=(3,3))
-    #         cv.Rodrigues(rvec, rotation_matrix)
-
-    cv.imshow('det_3d', img_det)
-    while True:
-        key = cv.waitKey(100)
-        if key == ord('q'):
-            break
+image_collector = StampedImagesCollector(img_topic, mode='video', rate=rate)
+image_collector.start_listen()
 
 
-# while cap.grab():
-#     retval, img = cap.retrieve()
-#     if retval:
-#         (corners, ids, rejected) = cv.aruco.detectMarkers( img, aruco_dict,
-#             parameters=aruco_params)
 
-#         cv.imshow('img from cam', img)
-#     else:
-#         print('can\'t get image, skip')
+# while True:
+#     print('video')
+#     if mode == 'video': 
+#         retval, img = cap.retrieve()
+#         print(img.shape)
+#         cv.imshow('cam', img)
+
+#     (corners, ids, rejected) = cv.aruco.detectMarkers( img, aruco_dict,
+#                 parameters=aruco_params)
+
+#     aruco_pts = np.zeros((4,3), dtype=np.float32)
+#     aruco_pts[0] = [-markerLength/2, markerLength/2, 0]
+#     aruco_pts[1] = [markerLength/2, markerLength/2, 0]
+#     aruco_pts[2] = [markerLength/2, -markerLength/2, 0]
+#     aruco_pts[3] = [-markerLength/2, -markerLength/2, 0]
+
+#     img_det = img.copy()
+#     if (ids is not None):
+#         img_det = cv.aruco.drawDetectedMarkers(img, corners, ids)
+#         cv.imshow('det', img_det)
+
+#         nMarkers = ids.size
+#         aruco_pos_arr = []
+#         aruco_rot_arr = []
+#         aruco_T_arr = []
+#         for i, c in enumerate(corners):
+#             retval, rvec, tvec = cv.solvePnP( aruco_pts, c, intrinsics, dist_coeffs)	
+#             if retval:
+#                 rot = np.zeros((3,3))
+#                 cv.Rodrigues(rvec, rot)
+#                 aruco_pos_arr.append(tvec)
+#                 aruco_rot_arr.append(rvec)
+#                 T = np.zeros((4,4))
+#                 T[:3,:3] = rot
+#                 T[:3,3] = tvec.reshape((3,))
+#                 T[3,3] = 1
+#                 aruco_T_arr.append(T)
+#                 # rvecs, tvecs, _ = cv.aruco.estimatePoseSingleMarkers( corners, 0.18, intrinsics, dist_coeffs )
+#                 print(f'tvec: {tvec}, rvec: {rvec}')
+#                 img_det = cv.drawFrameAxes(img_det, intrinsics, dist_coeffs, rvec, tvec, 0.1)
+#             # corners_galery_rel_galery = np.array([ [[galery_map_size[0] + padding_size], [galery_map_size[1] + padding_size], [0.]],
+#             #                                        [[-padding_size], [galery_map_size[1] + padding_size], [0.]],
+#             #                                        [[-padding_size], [-padding_size], [0.]],
+#             #                                        [[galery_map_size[0] + padding_size], [-padding_size], [0.]] ])
+
+#         # X rot
+#         Rot_ArW = np.array([[1, 0, 0],
+#                             [0, 0, 1],
+#                             [0, -1, 0]])
+        
+#         Trans_ArW = np.array([[-0.1,-0.1, 0.2]])
+#         # Trans_ArW = np.array([[0,0,0]])
+#         T_ArW = np.zeros((4,4))
+#         T_ArW[:3,:3] = Rot_ArW
+#         T_ArW[:3,3] = Trans_ArW
+#         T_ArW[3,3] = 1
+#         aruco_rot_m = np.zeros((3,3))
+#         # cv.Rodrigues(aruco_rot_arr[2], aruco_rot_m)
+#         # rvec_world =  aruco_rot_m @ aruco_to_world_rot_m
+#         T_wc = aruco_T_arr[3] @ T_ArW
+
+#         # print(rvec_world)
+#         # img_det = cv.drawFrameAxes(img_det, intrinsics, dist_coeffs, rvec_world, aruco_pos_arr[2], 0.1)
+#         img_det = cv.drawFrameAxes(img_det, intrinsics, dist_coeffs, T_wc[:3,:3], T_wc[:3,3], 0.1)
+
+#         # if rvecs is not None :
+#         #     for rvec, tvec, id in zip(rvecs, tvecs, ids) :
+#         #         rotation_matrix = np.zeros(shape=(3,3))
+#         #         cv.Rodrigues(rvec, rotation_matrix)
+
+#     cv.imshow('det_3d', img_det)
 
 #     key = cv.waitKey(100)
 #     if key == ord('q'):
 #         break
-
-
-
-# // Set coordinate system
-# cv.Mat aruco_pts (4, 1, CV_32FC3);
-# aruco_pts .ptr<cv.Vec3f>(0)[0] = cv.Vec3f(-markerLength/2.f, markerLength/2.f, 0);
-# aruco_pts .ptr<cv.Vec3f>(0)[1] = cv.Vec3f(markerLength/2.f, markerLength/2.f, 0);
-# aruco_pts .ptr<cv.Vec3f>(0)[2] = cv.Vec3f(markerLength/2.f, -markerLength/2.f, 0);
-# aruco_pts .ptr<cv.Vec3f>(0)[3] = cv.Vec3f(-markerLength/2.f, -markerLength/2.f, 0);
-# cv.aruco.DetectorParameters detectorParams = cv.aruco.DetectorParameters();
-# cv.aruco.Dictionary dictionary = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_6X6_250);
-# aruco.ArucoDetector detector(dictionary, detectorParams);
-# while (inputVideo.grab()) {
-#     cv.Mat image, imageCopy;
-#     inputVideo.retrieve(image);
-#     image.copyTo(imageCopy);
-#     std.vector<int> ids;
-#     std.vector<std.vector<cv.Point2f>> corners;
-#     detector.detectMarkers(image, corners, ids);
-#     // If at least one marker detected
-#     if (ids.size() > 0) {
-#         cv.aruco.drawDetectedMarkers(imageCopy, corners, ids);
-#         int nMarkers = corners.size();
-#         std.vector<cv.Vec3d> rvecs(nMarkers), tvecs(nMarkers);
-#         // Calculate pose for each marker
-#         for (int i = 0; i < nMarkers; i++) {
-#             solvePnP(aruco_pts , corners.at(i), cameraMatrix, distCoeffs, rvecs.at(i), tvecs.at(i));
-#         }
-#         // Draw axis for each marker
-#         for(unsigned int i = 0; i < ids.size(); i++) {
-#             cv.drawFrameAxes(imageCopy, cameraMatrix, distCoeffs, rvecs[i], tvecs[i], 0.1);
-#         }
-#     }
-#     // Show resulting image and close window
-#     cv.imshow("out", imageCopy);
-#     char key = (char) cv.waitKey(waitTime);
-#     if (key == 27)
-#         break;
-# }
